ea2fea7d436cd2a813837b957245d561c8d7fefb
Peter MEUEL <peter@teriiehina.net>
2015-01-07 23:10:04 +0100

    Add link at bottom of page

--- PageWrangler.safariextension/add_button.js
+++ PageWrangler.safariextension/add_button.js
@@ -51,14 +51,22 @@ var teriiehina_mark_this_page = function()
 {    
     var elements      = document.getElementsByClassName("stream_item");
     var elementsCount = elements.length;
-    
+        
     for (var bullet = 0; bullet < elementsCount; bullet++) 
     {
         var elementId = elements[bullet].getAttribute("id");
         elementId     = elementId.replace("-block" , "");
-        
+
         teriiehina_mark_read(elementId);
     }
+    
+    buttonTop.textContent = "Mark This Page";
+}
+
+function sleep(ms) 
+{
+    var unixtime_ms = new Date().getTime();
+    while(new Date().getTime() < unixtime_ms + ms) {}
 }
 
 var teriiehina_toggle_this_page = function()
@@ -76,28 +84,32 @@ var teriiehina_toggle_this_page = function()
 }
 
 
-var editButton    = document.getElementsByClassName("edit-button")[0];
+var editButtonTop    = document.getElementsByClassName("edit-button")[0];
+var editButtonBottom = document.getElementsByClassName("edit-button-bottom")[0];
 
 var sep1          = document.createElement("span");
 sep1.textContent  = " | ";
 
-var sep2          = document.createElement("span");
-sep2.textContent  = " | ";
+var sepBottom         = document.createElement("span");
+sepBottom.textContent = " | ";
 
 var mtpas         = document.createElement("a");
 mtpas.href        = "#";
 mtpas.onclick     = teriiehina_mark_this_page;
 mtpas.textContent = "Mark This Page";
+mtpas.id          = "mark_this_page_top";
 
-var ttpas         = document.createElement("a");
-ttpas.href        = "#";
-ttpas.onclick     = teriiehina_toggle_this_page;
-ttpas.textContent = "Toggle This Page";
+var linkBottom         = document.createElement("a");
+linkBottom.href        = "#";
+linkBottom.onclick     = teriiehina_mark_this_page;
+linkBottom.textContent = "Mark This Page";
+linkBottom.id          = "mark_this_page_bottom";
 
-editButton.appendChild(sep1);
-editButton.appendChild(mtpas);
-editButton.appendChild(sep2);
-editButton.appendChild(ttpas);
+editButtonTop.appendChild(sep1);
+editButtonTop.appendChild(mtpas);
+
+editButtonBottom.appendChild(sepBottom);
+editButtonBottom.appendChild(linkBottom);
 
 
 
